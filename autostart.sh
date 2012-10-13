09bc181798797216eee0ff68ab2bcec2da454f27
suem <samuel.ueltschi@gmail.com>
2012-10-13 18:09:48 +0200

    autostart script

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+pgrep nm-applet >> /dev/null || nm-applet &
+pgrep nm-applet >> /dev/null || volumeicon &
+# batti &
+
+/usr/bin/xset b off
+
+xrdb -merge ~/.Xresources
+
+xcape
